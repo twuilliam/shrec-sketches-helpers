ae208e0fc514abeb0312eae6c389c6a86a3ffde1
twuilliam <t.wil.pmtl@gmail.com>
2020-07-24 18:33:37 +0200

    rendering

---
+++ render.sh
@@ -0,0 +1,5 @@
+/your/path/blender-2.79b-linux-glibc219-x86_64/blender render_shaded_black_bg.blend \
+    --background --python render.py -- labels/SHREC14/cad.txt /data/path/
+
+/your/path/blender-2.79b-linux-glibc219-x86_64/blender render_shaded_black_bg.blend \
+    --background --python render.py -- labels/SHREC13/cad.txt /data/path/
